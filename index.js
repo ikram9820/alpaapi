8d5eda1ed08ceec75ecac24c372b863429db1d62
unknown <ikram9820khan@gmail.com>
2023-03-07 16:27:03 +0500

    chat app is added client render from server

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const winston = require("winston");
-const config = require('config')
+const path = require("path");
+
+const config = require("config");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const publicDirectoryPath = path.join(__dirname, "./public");
+app.use(express.static(publicDirectoryPath));
 
 // require("./startup/logging")();
 require("./startup/cors")(app);
@@ -11,10 +20,7 @@ require("./startup/db")();
 require("./startup/validation")();
 require("./startup/config")();
 
+require("./socketio/chat")(io);
 
 const port = process.env.PORT || config.get("port");
-const server = app.listen(port, () =>
-winston.info(`Listening on port ${port}...`)
-);
-
-module.exports = server;
+server.listen(port, () => winston.info(`Listening on port ${port}...`));
---
+++ socketio/chat.js
@@ -0,0 +1,66 @@
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+
+module.exports =  function (io){
+  io.on("connection", (socket) => {
+    console.log("New WebSocket connection");
+
+    socket.on("join", (options, callback) => {
+      const { error, user } = addUser({ id: socket.id, ...options });
+
+      if (error) {
+        return callback(error);
+      }
+
+      socket.join(user.room);
+
+      socket.emit("message", generateMessage("Admin", "Welcome!"));
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          "message",
+          generateMessage("Admin", `${user.username} has joined!`)
+        );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+
+      callback();
+    });
+
+    socket.on("sendMessage", (message, callback) => {
+      console.log(message)
+      const user = getUser(socket.id);
+      io.to(user.room).emit("message", generateMessage(user.username, message));
+      callback();
+    });
+
+    socket.on("sendLocation", (coords, callback) => {
+      const user = getUser(socket.id);
+      io.to(user.room).emit(
+        "locationMessage",
+        generateLocationMessage(
+          user.username,
+          `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+        )
+      );
+      callback();
+    });
+
+    socket.on("disconnect", () => {
+      const user = removeUser(socket.id);
+
+      if (user) {
+        io.to(user.room).emit(
+          "message",
+          generateMessage("Admin", `${user.username} has left!`)
+        );
+        io.to(user.room).emit("roomData", {
+          room: user.room,
+          users: getUsersInRoom(user.room),
+        });
+      }
+    });
+  });
+};
